3a1cd0d4eece177e44dfbee7696442377c3cdeb7
SoftandWare <geko2000@gmx.de>
2022-04-10 01:57:31 +0200

    Update Program.java

--- src/Program.java
+++ src/Program.java
@@ -4,40 +4,38 @@ public class Program {
     public static void main(String[] args) {
 
 
-        boolean a = isWellSorted(new String[] {"A", "B", "D", "C"});
+        boolean a = isWellSorted(new String[] {"A", "C", "D"});
 
         System.out.println(a);
     }
 
-    public static boolean isWellSorted( String[] sequence ){
+    public static boolean isWellSorted( String[] sequence ) {
+
         String order[][] = new String[][]{{ "A" , "C" }, { "C", "D" }, { "B", "C" }};
         String temp[][] = order;
-        String temp2;
-        int appearance;
+        int noAppearance;
 
         for (int i = 0; i < sequence.length; i++) {
-            appearance = 0;
+
+            noAppearance = 0;
+
             for (int j = 0; j < order.length; j++) {
 
-                if (sequence[i] != order[j][1])
-                {
-                    appearance++;
+                if (sequence[i] != order[j][1]) {
 
+                    noAppearance++;
                 }
-                if (appearance == temp.length)
-                {
+                if (noAppearance == temp.length) {
+
                     for (int k = 0; k < temp.length; k++) {
-                        if (temp[k][0] == sequence[i]){
-                            temp[k][1] = "no";
+
+                        if (temp[k][0] == sequence[i]) {
+                            temp[k][1] = null;
                         }
                     }
-
                 }
-
             }
-        }
-        for (int i = 0; i < temp.length; i++) {
-            if (temp[i][1] != "no"){
+            if (noAppearance != temp.length) {
                 return false;
             }
         }
