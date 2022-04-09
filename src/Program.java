a61ecbd190432c153e6166204142ccb900e3413d
SoftandWare <geko2000@gmx.de>
2022-04-09 21:58:59 +0200

    Merge branch 'main' of https://github.com/JoeTwizzle/TDD-Java

--- src/Program.java
+++ src/Program.java
@@ -3,10 +3,46 @@
 public class Program {
     public static void main(String[] args) {
 
+
+        boolean a = isWellSorted(new String[] {"A", "B", "D", "C"});
+
+        System.out.println(a);
     }
 
-    boolean isWellSorted( String[] sequence ){
-        return false;
+    public static boolean isWellSorted( String[] sequence ){
+        String order[][] = new String[][]{{ "A" , "C" }, { "C", "D" }, { "B", "C" }};
+        String temp[][] = order;
+        String temp2;
+        int appearance;
+
+        for (int i = 0; i < sequence.length; i++) {
+            appearance = 0;
+            for (int j = 0; j < order.length; j++) {
+
+                if (sequence[i] != order[j][1])
+                {
+                    appearance++;
+
+                }
+                if (appearance == temp.length)
+                {
+                    for (int k = 0; k < temp.length; k++) {
+                        if (temp[k][0] == sequence[i]){
+                            temp[k][1] = "no";
+                        }
+                    }
+
+                }
+
+            }
+        }
+        for (int i = 0; i < temp.length; i++) {
+            if (temp[i][1] != "no"){
+                return false;
+            }
+        }
+
+        return true;
     }
 
 
