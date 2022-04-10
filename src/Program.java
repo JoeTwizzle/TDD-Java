291418e5dea1b8d1918b7460ad7d37b50c02be7c
SoftandWare <geko2000@gmx.de>
2022-04-10 19:35:56 +0200

    Class implementation

--- src/Program.java
+++ src/Program.java
@@ -4,56 +4,8 @@ public class Program {
 
     }
 
-    public static boolean isWellSorted(String[] sequence) {
 
-        String[][] order = new String[][]{{"A", "C"}, {"C", "D"}, {"B", "C"}};
-        int noAppearance;
-
-        for (String s : sequence) {
-
-            boolean isContained = false;
-
-            for (int i = 0; i < order.length; i++) {
-
-                for (int j = 0; j < order[i].length; j++) {
-
-                    isContained |= order[i][j].equals(s);
-                }
-            }
-            if (!isContained) {
-
-                return false;
-            }
-        }
-
-
-        for (int i = 0; i < sequence.length; i++) {
-
-            noAppearance = 0;
-
-            for (int j = 0; j < order.length; j++) {
-
-                if (!sequence[i].equals(order[j][1])) {
-
-                    noAppearance++;
-                }
-                if (noAppearance == order.length) {
-
-                    for (int k = 0; k < order.length; k++) {
-
-                        if (order[k][0].equals(sequence[i])) {
-                            order[k][1] = null;
-                        }
-                    }
-                }
-            }
-            if (noAppearance != order.length) {
-                return false;
-            }
-        }
-
-        return true;
-    }
 
 
 }
+
