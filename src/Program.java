83b40a93690c48db1629b02f9815b3c0cf300e2f
SoftandWare <geko2000@gmx.de>
2022-04-10 19:23:06 +0200

    Update Program.java

--- src/Program.java
+++ src/Program.java
@@ -4,21 +4,20 @@ public class Program {
     public static void main(String[] args) {
 
 
-        boolean a = isWellSorted(new String[] {""});
+        boolean a = isWellSorted(new String[]{"A", "B", "C"});
 
         System.out.println(a);
     }
 
-    public static boolean isWellSorted( String[] sequence ) {
+    public static boolean isWellSorted(String[] sequence) {
 
-        String[][] order = new String[][]{{ "A", "C" }, { "C", "D" }, { "B", "C" }};
-        String[][] temp = order;
+        String[][] order = new String[][]{{"A", "C"}, {"C", "D"}, {"B", "C"}};
         boolean legit = true;
         int noAppearance;
 
         for (int i = 0; i < sequence.length; i++) {
 
-            if (legit == false){
+            if (legit == false) {
                 return false;
             }
 
@@ -28,8 +27,7 @@ public class Program {
 
                     legit = true;
                     break;
-                }
-                else {
+                } else {
 
                     legit = false;
 
@@ -37,7 +35,7 @@ public class Program {
             }
 
         }
-        if (legit == false){
+        if (legit == false) {
             return false;
         }
 
@@ -52,17 +50,17 @@ public class Program {
 
                     noAppearance++;
                 }
-                if (noAppearance == temp.length) {
+                if (noAppearance == order.length) {
 
-                    for (int k = 0; k < temp.length; k++) {
+                    for (int k = 0; k < order.length; k++) {
 
-                        if (temp[k][0].equals(sequence[i])) {
-                            temp[k][1] = null;
+                        if (order[k][0].equals(sequence[i])) {
+                            order[k][1] = null;
                         }
                     }
                 }
             }
-            if (noAppearance != temp.length) {
+            if (noAppearance != order.length) {
                 return false;
             }
         }
