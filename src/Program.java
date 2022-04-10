0ac6af3e2cee5049e7b3c6234ad4fffe4ffa83c9
SoftandWare <geko2000@gmx.de>
2022-04-10 19:14:19 +0200

    Update Program.java

--- src/Program.java
+++ src/Program.java
@@ -4,16 +4,43 @@ public class Program {
     public static void main(String[] args) {
 
 
-        boolean a = isWellSorted(new String[] {"A", "C", "D"});
+        boolean a = isWellSorted(new String[] {""});
 
         System.out.println(a);
     }
 
     public static boolean isWellSorted( String[] sequence ) {
 
-        String order[][] = new String[][]{{ "A" , "C" }, { "C", "D" }, { "B", "C" }};
+        String order[][] = new String[][]{{ "A", "C" }, { "C", "D" }, { "B", "C" }};
         String temp[][] = order;
+        boolean legit = true;
         int noAppearance;
+
+        for (int i = 0; i < sequence.length; i++) {
+
+            if (legit == false){
+                return false;
+            }
+
+            for (int j = 0; j < order.length; j++) {
+
+                if (sequence[i].equals(order[j][0]) || sequence[i].equals(order[j][1])) {
+
+                    legit = true;
+                    break;
+                }
+                else {
+
+                    legit = false;
+
+                }
+            }
+
+        }
+        if (legit == false){
+            return false;
+        }
+
 
         for (int i = 0; i < sequence.length; i++) {
 
@@ -21,7 +48,7 @@ public class Program {
 
             for (int j = 0; j < order.length; j++) {
 
-                if (sequence[i] != order[j][1]) {
+                if (!sequence[i].equals(order[j][1])) {
 
                     noAppearance++;
                 }
@@ -29,7 +56,7 @@ public class Program {
 
                     for (int k = 0; k < temp.length; k++) {
 
-                        if (temp[k][0] == sequence[i]) {
+                        if (temp[k][0].equals(sequence[i])) {
                             temp[k][1] = null;
                         }
                     }
